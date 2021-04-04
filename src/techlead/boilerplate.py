76fbf435fe62720dd75a45d623ad7c086d42a114
nabaei17@gmail.com <nabaei17@gmail.com>
2021-04-03 21:09:43 -0700

    add

--- src/techlead/boilerplate.py
+++ src/techlead/boilerplate.py
@@ -19,3 +19,13 @@ print(Solution().canSpell(['a','a', 'b', 'c', 'd', 'e', 'f'], 'cat'))
 
 arr = [0] * 26
 print(ord('z'))
+
+# to make hashed map with collection
+# word = "ab" or "abc" ...
+# groups= collections.defaultdict(list)
+# groups[word].append(word)
+# groups["".join(sorted(word))].append(word)
+# groups = {
+#   "abc": ['abc', 'cab', 'bca'],
+#   "ab": ['ba']
+# }
