32f2faf98c8dd968857854cfd4f254634893037e
amirnabaei <87382930+amirnabaei@users.noreply.github.com>
2023-01-30 10:44:12 -0800

    add

--- test.js
+++ test.js
@@ -14,3 +14,45 @@ console.log(person, a);
 console.log(age);
 
 
+const message = "AA"
+
+const object = {
+    message: 'Hello, World!',
+    getMessage() {
+      const message = 'Hello, Earth!';
+      return this.message;
+    },
+    newone: params => {
+        const message = 'Hello, Earth!';
+        return this;
+      }
+  };
+
+aaa = function() {
+    console.log("sss")
+    }
+
+console.log(aaa())
+console.log(object.newone());
+
+console.log("__________________________________________")
+
+function Pet(name) {
+    this.name = name;
+    // this.getName = () => this.name;
+    function getName() {
+        console.log("sss")
+    }
+  }
+  const cat = new Pet('Fluffy');
+  console.log(cat.getName())
+  
+//   console.log(cat.getName()); // What is logged?
+  
+  
+//   const { getName } = cat;
+//   console.log(getName());  
+
+
+
+
